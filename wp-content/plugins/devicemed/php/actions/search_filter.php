c93f3a79b7bd468845cf1ed1029d2688b0d18b49
Gilles François <gilles@sofoot.com>
2016-06-06 16:32:50 +0200

    bug du filtre recherche

--- wp-content/plugins/devicemed/php/actions/search_filter.php
+++ wp-content/plugins/devicemed/php/actions/search_filter.php
@@ -1,6 +1,6 @@
 <?php
 function search_filter($query) {
-	if ($query->is_search && $_GET['post_type']) {
+	if (!is_admin() && $query->is_search && $_GET['post_type']) {
 		$query->set('post_type', explode(',',$_GET['post_type']));
 	}
 	return $query;
