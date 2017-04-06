3d6ef92fee842fb169e253484da77644a7bb0c6e
Gilles FRANCOIS <gilles@sopress.net>
2017-04-06 14:24:40 +0200

    bug

--- wp-content/plugins/devicemed/php/actions/row_actions.php
+++ wp-content/plugins/devicemed/php/actions/row_actions.php
@@ -4,7 +4,7 @@ add_filter('post_row_actions','custion_action_row', 10, 2);
 
 function custion_action_row($actions, $post){
     if ($post->post_type =="post"){
-    	$actions['ga']='<a href="https://analytics.google.com/analytics/web/#report/content-drilldown/a55916994w89227602p92714496/%3Fexplorer-table.filter%3D%2F'.$post->ID.'%26explorer-table.plotKeys%3D%5B%5D/" target="_blank">Google Analytics</a>';
+    	$actions['ga']='<a href="https://analytics.google.com/analytics/web/#report/content-drilldown/a55916994w89227602p92714496/%3Fexplorer-table.plotKeys%3D%5B%5D%26explorer-table.filter%3D%2F'.$post->ID.'%26explorer-segmentExplorer.segmentId%3Danalytics.pagePath/" target="_blank">Google Analytics</a>';
     }
     return $actions;
 }
