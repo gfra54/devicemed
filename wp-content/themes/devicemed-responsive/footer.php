e2627c795fa79f4e7b169a982d1abaf5bc93038f
SOPRESS <gilles@sopress.net>
2019-04-24 09:35:10 +0200

    vogel

--- wp-content/themes/devicemed-responsive/footer.php
+++ wp-content/themes/devicemed-responsive/footer.php
@@ -2,7 +2,7 @@
 	</div><!-- .column-content -->
 
 <footer id="footer">
-	<div class="copyright">DeviceMed.fr est une marque de Vogel Business Media. <a href='http://www.devicemed.de/' target='_blank'>Cliquer ici pour découvrir le site de DeviceMed Allemagne</a>.</div>
+	<div class="copyright">DeviceMed.fr est une marque de Vogel Communications Group. <a href='http://www.devicemed.de/' target='_blank'>Cliquer ici pour découvrir le site de DeviceMed Allemagne</a>.</div>
 	<div class="pages"><?php devicemed_footer_menu('Bas de page - Première ligne'); ?></div>
 	<div class="credits"><?php devicemed_footer_menu('Bas de page - Deuxième ligne'); ?></div>
 </footer>
