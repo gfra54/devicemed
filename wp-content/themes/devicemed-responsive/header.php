e5f9551b61d385e05c66ddc9d693260107696e34
Gilles FRANCOIS <gilles@sopress.net>
2020-06-29 08:53:26 +0200

    google tag manager

--- wp-content/themes/devicemed-responsive/header.php
+++ wp-content/themes/devicemed-responsive/header.php
@@ -101,6 +101,14 @@ header( 'Cache-Control: max-age=604800' ); ?><!DOCTYPE html>
 	</script>
 	<script type='text/javascript' src='//platform-api.sharethis.com/js/sharethis.js#property=5bfd05d99b95fc00123f9c34&product=inline-share-buttons' async='async'></script>
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TQ544CQ');</script>
+<!-- End Google Tag Manager -->
+
 	<?php wp_head(); ?>
 </head>
 <div class='notConnected'>
@@ -121,6 +129,10 @@ header( 'Cache-Control: max-age=604800' ); ?><!DOCTYPE html>
 	>
 <?php } désactivé lors du passage a dvanced ads car pas utilisé jusque la*/?>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQ544CQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <div class='bloc_cachepopup'></div>
 <div class="container">
